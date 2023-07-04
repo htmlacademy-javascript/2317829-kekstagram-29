8430d58306bbfe3fe9e956adbeaf5ea151dcdd0b
Keks <keks@htmlacademy.ru>
2023-07-04 19:54:08 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/thumbnail-photos.js
@@ -0,0 +1,36 @@
+import { showBigPhoto } from './full-size-photos.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const templateFragment = document.querySelector('#picture').content;
+const template = templateFragment.querySelector('a');
+
+const renderPhoto = ({ url, comments, likes, description }) => {
+  const photoElement = template.cloneNode(true);
+  const pictureImage = photoElement.querySelector('.picture__img');
+  const pictureComments = photoElement.querySelector('.picture__comments');
+  const pictureLikes = photoElement.querySelector('.picture__likes');
+
+  pictureImage.src = url;
+  pictureImage.alt = description;
+  pictureComments.textContent = comments.length;
+  pictureLikes.textContent = likes;
+
+  const onElementClick = (evt) => {
+    evt.preventDefault();
+    showBigPhoto({ url, comments, likes, description });
+  };
+
+  photoElement.addEventListener('click', onElementClick);
+  return photoElement;
+};
+
+const renderPhotos = (photos) => {
+  const fragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    const element = renderPhoto(photo);
+    fragment.appendChild(element);
+  });
+  picturesContainer.appendChild(fragment);
+};
+
+export { renderPhotos };
